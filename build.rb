ec8868650ff454d9ea47b6f8def17eb909a657a8
Sam Winter <sam.winter@gmail.com>
2014-03-10 09:24:31 -0400

    Playing with the build script.

--- build.rb
+++ build.rb
@@ -4,17 +4,24 @@ CC = 'gcc'
 FLAGS = '-std=c99'
 CMD = "#{CC} #{FLAGS}"
 
-puts `rm -f webby *.o`
-
-cmd = "#{CMD} -c server.c"
+cmd = "rm -f webby *.o"
 puts cmd
 puts `#{cmd}`
 
-cmd = `#{CMD} -c main.c`
-puts cmd
-puts `#{cmd}`
+# cmd = "#{CMD} -c server.c"
+# puts cmd
+# puts `#{cmd}`
+
+# cmd = `#{CMD} -c main.c`
+# puts cmd
+# puts `#{cmd}`
 
 objects = `ls *.o`
-cmd =`#{CMD} #{objects} -o webby`
-puts cmd
-puts `#{cmd}`
+
+if objects.empty?
+	puts "No .o files to compile."
+else
+	cmd =`#{CMD} #{objects} -o webby`
+	puts cmd
+	puts `#{cmd}`
+end
